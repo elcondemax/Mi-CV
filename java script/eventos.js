a7b18f861cbde2c296dc013d0f63078e92301fb4
elcondemax <elcondemax@hotmail.com>
2023-02-05 18:40:35 -0300

    se subió pagina cv

---
+++ java script/eventos.js
@@ -0,0 +1,99 @@
+function enAzul1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal2.png";
+    }
+    function enNegro1() {
+        Element =document.getElementById("pers");
+        Element.src="img/personal.png";
+    }
+    
+    function enAzul2() {
+        Element =document.getElementById("labo");
+        Element.src="img/laboral2.png";
+    }
+     function enNegro2() {
+        Element =document.getElementById("labo");
+        Element.src="img/laboral.png";
+    }
+    
+    function enAzul3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades2.png";
+    }
+    function enNegro3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades.png";
+    }
+    
+    function enAzul4() {
+        Element =document.getElementById("estu");
+        Element.src="img/estudios2.png";
+    }
+    function enNegro4() {
+        Element =document.getElementById("estu");
+        Element.src="img/estudios.png";
+    }
+    
+    function enAzul5() {
+        Element =document.getElementById("idi");
+        Element.src="img/idioma2.png";
+    }
+    function enNegro5() {
+        Element =document.getElementById("idi");
+        Element.src="img/idioma.png";
+    }
+
+    function enAzul6() {
+        Element =document.getElementById("ini");
+        Element.src="img/inicio2.png";
+    }
+    function enNegro6() {
+        Element =document.getElementById("ini");
+        Element.src="img/inicio.png";
+    }
+
+    function enAzul7() {
+        Element =document.getElementById("fe");
+        Element.src="img/face2.png";
+    }
+    function enNegro7() {
+        Element =document.getElementById("fe");
+        Element.src="img/face.png";
+    }
+
+    function enAzul8() {
+        Element =document.getElementById("ma");
+        Element.src="img/mail2.png";
+    }
+    function enNegro8() {
+        Element =document.getElementById("ma");
+        Element.src="img/mail.png";
+    }
+    
+    function enAzul9() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin2.png";
+    }
+    function enNegro9() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin.png";
+    }
+
+    function imprimirPagina() {
+        Element =document.getElementById(imprimirPagina)
+        window.print();
+    }
+
+    function abrirIn() {
+    Element =document.getElementById("li");
+    window.open("https://ar.linkedin.com/", "Diseño Web","width=500, height=500")
+    }
+
+    function enviarEmail() {
+    window.open("mailto:elcondemax@hotmail.com")
+    }
+
+    function abrirFace() {
+        Element =document.getElementById("li");
+        window.open("https://faceboo.com/max.koziol.3", "Diseño Web","width=500, height=500")        
+    }
